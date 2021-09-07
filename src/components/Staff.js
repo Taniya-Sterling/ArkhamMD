615a793a83f144885bfc6e8d4da6c59d1c30c38d
Taniya Sterling <sterlingtaniya@gmail.com>
2021-09-06 20:17:20 -0400

    md

---
+++ src/components/Staff.js
@@ -0,0 +1,64 @@
+import React, {useState, useEffect} from 'react';
+import fetchData from '../api/api'
+
+
+const StaffComponent = () => {
+
+    const [StaffList, setStaffList] = useState([]);
+    useEffect( async function (){
+        try{
+          const response = await fetchData('?results=5')
+          const data = await response.json();
+          console.log(data);
+        }catch (error) {
+          console.error(error);
+        }
+
+
+        const Staffelements = StaffList.map((Staff,i) =>     
+            <div key={`Staff-id-${i}`}>  
+            <p>Name: {staff.name.first} </p>
+            console.log(Staffelements)
+            </div>);
+
+
+    return(
+        <div>
+            <h1 id = 'Staff'> Staff </h1> 
+            <div  className = 'Staff-container'>
+            {Staffelements}
+            </div>
+        </div>
+        
+    )   
+    
+})}
+
+    export default (StaffComponent)
+/* <header>
+  <h1>ArkhamMD</h1>
+</header>
+
+<nav>
+  <ul>
+	  <li><a href="#">Login</a></li>
+	  <li><a href="#">HOME</a></li>
+	  <li><a href="#">Staff</a></li>
+    <li><a href="#">Patient info</a></li>
+  </ul>
+</nav>
+
+<section>
+  <h2>Section Title</h2>
+  <article class = 'staff box'>
+    <h3>Staff and Personnel </h3>
+    <p>lorem ipsum si imet delor</p>
+  </article>
+</section>
+
+<footer>
+  <p>&copy; 2014 Wayne Enterprises. All rights reserved.</p> -->
+</footer> */
+
+// <div id="app"></div>
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
 import fetchData from './api/api'
+import StaffComponent from './components/Staff';
 // import {
 //     BrowserRouter as Router,
 //     Link,
@@ -12,25 +13,17 @@ import fetchData from './api/api'
 // const [userTodos, setUserTodos] = useState([]);
 
 const App = () => {
-    const [users, setUsers] = useState([]);
-    fetchData('?results=5')
-    // .then(users => {
-    //   setUserList(users)})
-    // useEffect(() => {
-       
-    //       })
-    //       .catch(error => {
-    //         // something something errors
-    //       });
-    //   }, []);
-    
-
     return (
         <div>
-            <div>ArkhamMD</div>
+            return (
+            <div>
+            <StaffComponent/>
+            </div>
+            )   
             
         </div>
     )
+ 
 }
 
-ReactDOM.render(<App/>,document.getElementById('app'))
+ReactDOM.render(<App/>,document.getElementById('App'))
